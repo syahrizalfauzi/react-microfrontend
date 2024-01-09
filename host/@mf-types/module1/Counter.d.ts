14127e06ca9f571e1179de8da5c4f1d535c2f80a
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2024-01-09 16:47:23 +0700

    Initial commit

---
+++ host/@mf-types/module1/Counter.d.ts
@@ -0,0 +1,2 @@
+export * from './_types/counter/index';
+export { default } from './_types/counter/index';
---
+++ host/@mf-types/module1/Input.d.ts
@@ -0,0 +1,2 @@
+export * from './_types/input/index';
+export { default } from './_types/input/index';
